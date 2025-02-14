037dc9d4c485e1789191e341ce58b5f29579776e
sbeuran <samuel.beuran98@gmail.com>
2025-02-14 17:33:46 +0000

    fix: set explicit skip_final_snapshot and static final_snapshot_identifier [skip test]

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,8 +94,8 @@ resource "aws_rds_cluster" "main" {
   port                            = var.database_port
   backup_retention_period         = var.backup_retention_period
   preferred_backup_window         = "03:00-04:00"
-  skip_final_snapshot             = var.skip_final_snapshot
-  final_snapshot_identifier       = "${var.rds_cluster_identifier}-final-snapshot-${formatdate("YYYYMMDDHHmmss", timestamp())}"
+  skip_final_snapshot             = true
+  final_snapshot_identifier       = "dataops-hub-final-snapshot"
   deletion_protection             = var.deletion_protection
   storage_encrypted               = var.storage_encrypted
   enabled_cloudwatch_logs_exports = ["postgresql"]
